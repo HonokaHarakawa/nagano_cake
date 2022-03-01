47f84f45d9c6a6937b17d5c42f30f35ce7bffc26
HonokaHarakawa <wow.hono313@gmail.com>
2022-03-01 10:34:13 +0000

    [finish]商品編集画面

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7d16a6213547f495eb4b568627730fe05fde99f2bd63f752de83c49c206a16e5c36599a73465a086c3c474f5d24ceaf18f46d9f4c8e30867439ede3ebe69bf34'
