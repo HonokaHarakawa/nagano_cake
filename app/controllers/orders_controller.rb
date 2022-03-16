711d9d52aee78ec1982e04986cd3eb3e1815c881
HonokaHarakawa <wow.hono313@gmail.com>
2022-03-16 13:09:26 +0000

    [finish]注文情報入力画面

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,12 +10,17 @@ class OrdersController < ApplicationController
   
   def confirm
     @order = Order.new(order_params)
-    @order.postal_code = current_customer.postal_code
-    @order.address = current_customer.address
-    @order.name = current_customer.first_name + current_customer.last_name
-    @order.save!
-    redirect_to  orders_confirm_path
-    binding.pry
+    if params[:order][:select_address] == "0"
+      @order.postal_code = current_customer.postal_code
+      @order.address = current_customer.address
+      @order.name = current_customer.first_name + current_customer.last_name
+      @order.save
+    elsif select_address == "1"
+      @order = params[:order][:address_id]
+      @order.save
+    else  
+      @order.save
+    end    
   end 
   
   def thanks
